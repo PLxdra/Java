8e229b879fbf3afdd88d2171eb8d9da9ea47c4fb
PLxdra <xpuntus@gmail.com>
2017-09-18 19:13:40 +0300

    Clean up

--- Student.java
+++
@@ -1,9 +0,0 @@
-public class Student {
-    String name;
-    String lastName;
-
-    public Student(String name, String lastName) {
-        this.name = name;
-        this.lastName = lastName;
-    }
-}
