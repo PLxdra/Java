38f383bfdb387df59e2946bc4f14a7d85761e78f
PLxdra <xpuntus@gmail.com>
2017-09-18 19:12:00 +0300

    Task2 is added

---
+++ Task2/src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    String name;
+    String lastName;
+
+    public Student(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
